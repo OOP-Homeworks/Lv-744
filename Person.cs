05079d2850f9f08daaeecabd30e628a6a33ea5d6
MaxPanenko <121324685+MaxPanenko@users.noreply.github.com>
2023-02-06 14:28:52 +0100

    HomeWork-3

---
+++ Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftServe
+{
+    internal class Person
+    {
+        private string name;
+        private int birthYears;
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public int Years
+        {
+            get { return birthYears; }
+        }
+
+        public Person(string name, int birthYears)
+        {
+            this.name = name;
+            this.birthYears = birthYears;
+        }
+        public int Age()
+        {
+            return DateTime.Now.Year - birthYears;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter a name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter age: ");
+            birthYears = int.Parse(Console.ReadLine());
+        }
+        public void ChangeName()
+        {
+            Console.WriteLine("You can change your name: ");
+            name = Console.ReadLine();
+            if (birthYears < 16)
+            {
+                Console.WriteLine("Very Young");
+            }
+        }
+        public override string ToString()
+        {
+            return $"{name},{birthYears}";
+        }
+        public void Output()
+        {
+            ToString();
+        }
+        public Person() 
+        {
+
+        }
+
+        public static bool  operator ==(Person person, Person person1) 
+        {
+            return person.Name == person1.Name;
+
+        }
+        public static bool operator !=(Person name, Person name1)
+        {
+            return !(name.Name != name1.Name);
+        }
+
+
+
+
+
+
+    }
+}
