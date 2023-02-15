2a9afc34d1df11522c1bb436adbf2eac7dfda74a
OleksSavchenko <98595558+OleksSavchenko@users.noreply.github.com>
2023-02-15 19:00:04 +0200

    Add files via upload

---
+++ Homework8-SavchenkoOleks/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8_SavchenkoOleks_LV744
+{
+    internal class Circle : Shape
+    {
+        private double radius;
+        public Circle(string name, double radius) : base(name)
+        {
+            this.radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
---
+++ Homework8-SavchenkoOleks/Shape.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8_SavchenkoOleks_LV744
+{
+    abstract class Shape : IComparable<Shape>
+    {
+        private string name;
+        public string Name { get { return name; } }
+        public Shape(string name)
+        {
+            this.name = name;
+        }
+        public abstract double Area();
+        public abstract double Perimeter();
+        public void Print()
+        {
+            Console.WriteLine("Shape is {0:}, area is {1:f2}.", name, Area());
+        }
+
+        public int CompareTo(Shape? s)
+        {
+            Circle circle = s as Circle;
+            Square square = s as Square;
+            if(circle != null)
+            {
+                return this.Area().CompareTo(circle.Area());
+            }
+            else if(square != null)
+            {
+                return this.Area().CompareTo(square.Area());
+            }
+            else
+            {
+                throw new ArgumentException("You're trying to sort not shape elements");
+            }
+        }
+    }
+}
