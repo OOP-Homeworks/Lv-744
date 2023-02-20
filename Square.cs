1ea97885e7dca0f0b18f20bc63c185e3e71d8e56
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2023-02-20 10:00:21 +0200

    AndriiFedoriv-HomeWork-9

--- Square.cs
+++ Square.cs
@@ -4,32 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HW_8_2023
+namespace _18._02._2023_HomeWork_9
 {
-    class Square : Shapes
+    internal class Square : Shape
     {
-        private int side;
-        private string size;
-        public int Side { get { return side; } }
-        public string Size { get { return size; } }
+        private double side;
 
-        public Square(string name, int side, string size) : base(name)
+        public Square(string name, double side) : base("Square")
         {
-            this.size = size;
             this.side = side;
         }
-        public override  double Area()
+
+        public override double Area()
         {
-            return side * side;   
+            return side * side;
         }
 
-        public override  double Perimeter()
+        public override double Perimeter()
         {
-            return 4 * side;    
-        }
-        public override void Print()
-        {
-            Console.WriteLine("Square method Print :\n{0} have Area : {1:###.##} and Perimeter :{2:###.##} Size : {3}",name,Area(),Perimeter(),size);
+            return 4 * side;
         }
     }
 }
