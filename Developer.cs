87eff81b4b6f3ca9bc921a1055da01598fd37c5f
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2023-02-20 09:49:36 +0200

    AndriiFedoriv-Task-8

---
+++ Developer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230215_HW_Task_8
+{
+    internal class Developer : Staff
+    {
+        public string LangProgam;
+        public Developer(string name, string langProgam, int salary) : base(name, salary)
+        {
+            LangProgam = langProgam;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("{0} - {1}: ${2}", Name, LangProgam, Salary);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Developer: {0} - '{1}'", Name, LangProgam);
+        }
+
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230215_HW_Task_8
+{
+    internal class Teacher : Staff
+    {
+        public string Position { get; set; }
+
+        public Teacher(string name, string position, int salary) : base(name, salary)
+        {
+            Position = position;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("{0} - {1}: ${2}", Name, Position, Salary);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Teacher: {0} - '{1}'", Name, Position);
+        }
+    }
+}
