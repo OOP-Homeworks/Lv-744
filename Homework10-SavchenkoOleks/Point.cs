26454a98b7e607f3a58eb9e67c922e26ccf80a1d
OleksSavchenko <98595558+OleksSavchenko@users.noreply.github.com>
2023-02-23 14:56:08 +0200

    Homework10

---
+++ Homework10-SavchenkoOleks/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10_SavchenkoOleks_LV744
+{
+    public struct Point
+    {
+        public double x;
+        public double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double Distance(double x1, double y1)
+        {
+            return Math.Sqrt(Math.Pow((x - x1), 2) + Math.Pow((y - y1), 2));
+        }
+        public double Distance(Point p)
+        {
+            return Math.Sqrt(Math.Pow((p.x - x), 2) + Math.Pow((p.y - y), 2));
+        }
+        public override string ToString()
+        {
+            return String.Format("({0},{1})", x, y);
+        }
+    }
+}
---
+++ Homework10-SavchenkoOleks/Triangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10_SavchenkoOleks_LV744
+{
+    public class Triangle
+    {
+        Point vertex1, vertex2, vertex3;
+
+        public Triangle(Point vertex1, Point vertex2, Point vertex3)
+        {
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.vertex3 = vertex3;
+        }
+
+        public double Perimeter()
+        {
+            return Math.Round(vertex1.Distance(vertex2) + vertex2.Distance(vertex3) + vertex3.Distance(vertex1), 2);
+        }
+
+        public bool IsTriangleCorrect()
+        {
+            double side1 = vertex1.Distance(vertex2);
+            double side2 = vertex2.Distance(vertex3);
+            double side3 = vertex3.Distance(vertex1);
+            if (Perimeter() - side1 <= side1 || Perimeter() - side2 <= side2 || Perimeter() - side3 <= side3) return false;
+            else return true;
+        }
+
+        public double Square()
+        {
+            double p = Perimeter() / 2;
+            double firstSide = vertex1.Distance(vertex2);
+            double secondSide = vertex2.Distance(vertex3);
+            double thirdSide = vertex3.Distance(vertex1);
+            return Math.Round(Math.Sqrt(p * (p - firstSide) * (p - secondSide) * (p - thirdSide)), 2);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Triangle with vertexes: {vertex1}, {vertex2}, {vertex3}" +
+                              $"Perimeter is {Perimeter()}, Square is {Square()}");
+        }
+    }
+}
