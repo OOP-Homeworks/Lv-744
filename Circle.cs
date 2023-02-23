20032c9fd89253a9ae590917eaee766b3d86428c
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2023-02-23 10:43:17 +0200

    AndriiFedoriv-HomeWork_9

--- Circle.cs
+++ Circle.cs
@@ -4,40 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _20230216_HomeWork_8
+namespace _18._02._2023_HomeWork_9
 {
     internal class Circle : Shape
     {
         private double radius;
 
-        public Circle(string name, double radius) : base(name)
-
+        public Circle(string name, double radius) : base("Circle")
         {
             this.radius = radius;
         }
 
-
         public override double Area()
         {
             return Math.PI * radius * radius;
-
-        public override double GetPerimeter()
-        {
-            return 2 * Math.PI * radius;
-
         }
 
-        public override double GetArea()
+        public override double Perimeter()
         {
-
             return 2 * Math.PI * radius;
-
-        }
-
-        public override string GetName()
-        {
-            return "Circle";
-
         }
     }
 }
