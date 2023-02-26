8b365f9b6322369b5fde847b6f7c8b735fbeb7c1
Danylo Pavlusenko <119690378+navarti@users.noreply.github.com>
2023-02-26 22:20:20 +0200

    Pavlusenko Homework 11

---
+++ PD_Accountancy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework.h_w
+{
+    internal class PD_Accountancy
+    {
+        static int minimalMark = 85;
+        public void PayingFellowship(int mark)
+        {
+            Console.WriteLine("Fellowship will " + (mark > minimalMark ?"":"not ") + "be paid");
+        }
+    }
+}
