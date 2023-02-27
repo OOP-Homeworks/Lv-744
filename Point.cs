5002550b4d566480314a0731b24b602500d5dcb1
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2023-02-27 10:13:16 +0200

    AndriiFedoriv-HomeWork_10

---
+++ Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23022023_HomeWork_10
+{
+    public class Point
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point() : this(0, 0) { }
+        
+        public override string ToString() => $"({X:F3}, {Y:F3})";
+
+        public double Distance(Point other)
+        {
+            return Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
+        }
+
+        public static double Distance(Point first, Point second)
+        {
+            return first.Distance(second);
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,171 +1,48 @@
-﻿using System.Collections.Concurrent;
-using System.Globalization;
-using System.Security.Cryptography;
-
-namespace Lecture6_744
+﻿namespace _23022023_HomeWork_10
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Task #1&2");
-            TaskHomework_1_2();
+            Random rnd = new Random();
+            const int sizeNum = 3;
+            Point start = new Point(0, 0);
+            double min = double.MaxValue;
+            int minTriangle = 0;
+            var list = new List<Triangle>();
 
-            Console.WriteLine("Task #3");
-            TaskHomework_3();
-
-            Console.ReadKey();
-
-        }
-
-        static void TaskHomework_1_2()
-        {
-            int i = 0;
-
-            while (i<=5)
+            for (int i = 0; i < sizeNum; i++)
             {
+                Point[] vertexes = new Point[3];
+
+                for (int j = 0; j < 3; j++)
+                {
+                    vertexes[j] = new Point(rnd.NextDouble(), rnd.NextDouble());
+                }
+
                 try
                 {
-                    Console.Write("Enter int a = ");
-                    string a_sting = Console.ReadLine().Replace(',', '.');
-                    Console.Write("Enter int b = ");
-                    string b_sting = Console.ReadLine().Replace(',', '.');
-
-                    NumberFormatInfo provider = new NumberFormatInfo();
-                    provider.NumberDecimalSeparator = ".";
-                    provider.NumberGroupSeparator = ",";
-
-                    double a_double = Convert.ToDouble(a_sting, provider);
-                    double b_double = Convert.ToDouble(b_sting, provider);
-
-                    if (a_double.ToString().Contains(',')==true && b_double.ToString().Contains(',') == true)
+                    var triangle = new Triangle(vertexes[0], vertexes[1], vertexes[2]);
+                    if (triangle.IsValid())
                     {
-                        throw new Exception("You enter double!");
-                    }
-
-                    int a = Int32.Parse(a_sting);
-                    int b = Int32.Parse(b_sting);
-                    int result = Div(a, b);
-                    Console.WriteLine($"{a} / {b} = {result}");
-                }
-                catch (DivideByZeroException exeption)
-                {
-                    Console.WriteLine("Div by 0 = " + exeption.Message);
-                }
-                catch (ArithmeticException exeption)
-                {
-                    Console.WriteLine("Another exeption = " + exeption.Message);
-                }
-                catch (FormatException exeption)
-                {
-                    Console.WriteLine("Format exeption = " + exeption.Message);
-                }
-                catch (Exception exeption)
-                {
-                    Console.WriteLine("Total exeption = " + exeption.Message);
-                }
-                finally
-                {
-                    Console.WriteLine($"Finally msg");
-                }
-
-                i++;
-
-            }
-
-        }
-
-        static void TaskHomework_3()
-        {
-            int min = 0;
-            int max = 0;
-
-            bool allIsOk = true;
-
-            try
-            {
-                Console.Write("Enter int Min = ");
-                min = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Enter int Max = ");
-                max = Convert.ToInt32(Console.ReadLine());
-
-                if (min > max)
-                {
-                    throw new Exception("Error! Min > Max!");
-                }
-
-            }
-            catch (FormatException exeption)
-            {
-                Console.WriteLine("Enter int numbers!");
-                allIsOk = false;
-            }
-            catch (Exception exeption)
-            {
-                Console.WriteLine(exeption.Message);
-                allIsOk = false;
-            }
-
-            if (allIsOk && min !=0 && max != 0)
-            {
-                var result = ReadNumber(min, max);
-                Console.WriteLine("Your range = ");
-                foreach(var item in result) { Console.WriteLine(item.ToString()); }
-            }
-
-        }
-
-        static int Div(int a, int b)
-        { return a / b; }
-
-        static int[] ReadNumber(int min, int max)
-        {
-            int totalSizeArray = 5;
-            int[] result = new int[totalSizeArray];
-
-            for (int i = 0; i < totalSizeArray; i++)
-            {
-                while (true)
-                {
-
-                    bool allIsCorrect = true;
-                    try
-                    {
-                        Console.Write($"Enter number in range #{i+1} = ");
-                        int v = Int32.Parse(Console.ReadLine());
-
-                        if (min < v && v < max)
+                        double distance = vertexes[0].Distance(start) + vertexes[1].Distance(start) + vertexes[2].Distance(start);
+                        if (distance < min)
                         {
-                            result[i] = v;
+                            min = distance;
+                            minTriangle = i;
                         }
-                        else
-                        {
-                            throw new Exception($"Error! The number must be in range {min} ... {max}!");
-                            allIsCorrect = false;
-                        }
-
+                        list.Add(triangle);
                     }
-                    catch (FormatException exeption)
-                    {
-                        Console.WriteLine("Format exeption = " + exeption.Message);
-                        allIsCorrect = false;
-                    }
-                    catch (Exception exeption)
-                    {
-                        Console.WriteLine("Total exeption = " + exeption.Message);
-                        allIsCorrect = false;
-                    }
-
-                    if(allIsCorrect) { break; }
-
+                }
+                catch (ArgumentException)
+                {
+                    i--;
                 }
             }
 
-            return result;
+            list[minTriangle].Print();
 
+            Console.ReadLine();
         }
-
     }
-
 }
